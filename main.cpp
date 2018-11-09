e3b6e9d167e9d9ba315ed3332806a72b14e0ef66
Jonathan Meldrum <meldrum.jon+git@gmail.com>
2018-11-08 22:20:11 -0700

    Ability to send and receive service headers!

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <Ticker.h>
 #include <WiFiUdp.h>
+
+#define DEBUG 1
 
 // Header
 #define LIFX_HEADER_TARGET_LEN 8 
+#define LIFX_HEADER_RESERVED_LEN 6
 // Header Constants
 #define LIFX_HEADER_PROTOCOL 1024 // Must be 1024
 #define LIFX_HEADER_ADDRESSABLE 1 // Indicates header includes target address, must be 1
@@ -17,48 +19,50 @@
 #define LIFX_HEADER_RES_REQ 0 // Have light send back an acknowledgement. Not used in this code.
 #define LIFX_HEADER_ACK_REQ 0 // Request a response from the light. Not used in this code.
 
+#define LIFX_MSG_GET_SERVICE 2
+
+#define LIFX_UDP_PORT 56700 // Can be changed for newer bulbs, best compatibility use 56700
+
 const char *WIFI_SSID = "125 2.4GHz";
 const char *WIFI_PASSWORD = "fivetimesfive";
-
-const uint8_t MAX_LX_DEVICES = 16;
-uint8_t LX_DEVICES = 0;
-
-const uint8_t SIZE_OF_MAC = 6;
-
-byte lxDevices[MAX_LX_DEVICES][SIZE_OF_MAC];
-IPAddress lxDevicesAddr[MAX_LX_DEVICES];
 
 IPAddress bcastAddr(255, 255, 255, 255);
 int lxPort = 56700;
 
 WiFiUDP UDP;
 
-uint8_t buttonPushed = 0;
-uint8_t buttonTimeout = 0;
-uint8_t interuptPin = 0;
+int8_t buttonPushed = -1;
 
 #pragma pack(push, 1)
 typedef struct {
-  /* frame */
-  uint16_t size;            // Size of entire message in bytes including this field
-  uint16_t protocol : 12;   // Protocol number: must be 1024 (decimal)
-  uint8_t addressable : 1;  // Message includes a target address: must be one (1)
-  uint8_t tagged : 1;       // Determines usage of the Frame Address target field
-  uint8_t origin : 2;       // Message origin indicator: must be zero (0)
-  uint32_t source;          // Source identifier: unique value set by the client, used by responses
-  /* frame address */
-  uint8_t target[8];        // 6 byte device address (MAC address) or zero (0) means all devices
-  uint8_t reserved[6];      // Must all be zero (0)
-  uint8_t res_required : 1; // Response message required
-  uint8_t ack_required : 1; // Acknowledgement message required
-  uint8_t : 6;              // Reserved
-  uint8_t sequence;         // Wrap around message sequence number
-  /* protocol header */
-  uint64_t : 64;            // Reserved
-  uint16_t type;            // Message type determines the payload being used
-  uint16_t : 16;            // Reserved
-  /* variable length payload follows */
+	/* frame */
+	uint16_t size;            // Size of entire message in bytes including this field
+	uint16_t protocol : 12;   // Protocol number: must be 1024 (decimal)
+	uint8_t addressable : 1;  // Message includes a target address: must be one (1)
+	uint8_t tagged : 1;       // Determines usage of the Frame Address target field
+	uint8_t origin : 2;       // Message origin indicator: must be zero (0)
+	uint32_t source;          // Source identifier: unique value set by the client, used by responses
+	/* frame address */
+	uint8_t target[8];        // 6 byte device address (MAC address) or zero (0) means all devices
+	uint8_t reserved[6];      // Must all be zero (0)
+	uint8_t res_required : 1; // Response message required
+	uint8_t ack_required : 1; // Acknowledgement message required
+	uint8_t : 6;              // Reserved
+	uint8_t sequence;         // Wrap around message sequence number
+	/* protocol header */
+	uint64_t : 64;            // Reserved
+	uint16_t type;            // Message type determines the payload being used
+	uint16_t : 16;            // Reserved
+	/* variable length payload follows */
 } lx_protocol_header_t;
+#pragma pack(pop)
+
+// Messages
+#pragma pack(push, 1)
+typedef struct {
+	uint8_t service;
+	uint32_t port;
+} lx_msg_stateService_t;
 #pragma pack(pop)
 
 // lx_protocol_header_t testHeader = {
@@ -70,238 +74,224 @@ typedef struct {
 
 #pragma pack(push, 1)
 typedef struct {
-  /* set power */
-  uint16_t level;
-  uint32_t duration;
+	/* set power */
+	uint16_t level;
+	uint32_t duration;
 } lx_set_power_t;
 #pragma pack(pop)
 
+
 byte packetBuffer[128];
-Ticker ticker;
 
-// Payload bytes: length of the actual message
-// Tagged: should be 1 unless discovering LIFX devices
-// TargetMAC: MAC address array
-// MessageType: ID of the message included in this packet
-void lxMakeFrame(lx_protocol_header_t *lxHead, uint8_t payloadBytes,
-                 uint8_t tagged, uint8_t *targetMAC, uint16_t messageType,
-                 uint8_t sequenceNum=0) {
-  /* frame */
-  lxHead->size = sizeof(lx_protocol_header_t) + payloadBytes;
-  lxHead->protocol = LIFX_HEADER_PROTOCOL; // Must be 1024
-  lxHead->addressable = LIFX_HEADER_ADDRESSABLE; // Must be 1
-  lxHead->tagged = tagged;
-  lxHead->origin = LIFX_HEADER_ORIGIN; // Must be 0
-  lxHead->source = LIFX_HEADER_SOURCE; // Identifies the ESP8266, LIFX bulb sends responses to this number
+// void lxDiscovery() {
+// 	/* Build lxDiscovery payload */
+// 	byte target_addr[8] = {0};
+// 	lx_protocol_header_t *lxHead;
+// 	lxHead = (lx_protocol_header_t *)calloc(1, sizeof(lx_protocol_header_t));
+// 	lxMakeFrame(lxHead, 0, 1, target_addr, 2);
 
-  /* frame address */
-  for (uint8_t i = 0; i < LIFX_HEADER_TARGET_LEN; i++) {
-    lxHead->target[i] = targetMAC[i];
-  }
-  lxHead->res_required = LIFX_HEADER_RES_REQ;
-  lxHead->ack_required = LIFX_HEADER_ACK_REQ;
-  lxHead->sequence = sequenceNum;
+// 	/* Start listening for responses */
+// 	UDP.begin(4097);
+// 	delay(500);
+// 	/* Send a couple of discovery packets out to the network*/
+// 	for (int i = 0; i < 1; i++) {
+// 	byte *b = (byte *)lxHead;
+// 	UDP.beginPacket(bcastAddr, lxPort);
+// 	UDP.write(b, sizeof(lx_protocol_header_t));
+// 	UDP.endPacket();
 
-  /* protocol header */
-  lxHead->type = messageType;
-}
+// 	delay(500);
+// 	}
 
-void lxDiscovery() {
-  /* Build lxDiscovery payload */
-  byte target_addr[8] = {0};
-  lx_protocol_header_t *lxHead;
-  lxHead = (lx_protocol_header_t *)calloc(1, sizeof(lx_protocol_header_t));
-  lxMakeFrame(lxHead, 0, 1, target_addr, 2);
+// 	free(lxHead);
 
-  /* Start listening for responses */
-  UDP.begin(4097);
-  delay(500);
-  /* Send a couple of discovery packets out to the network*/
-  for (int i = 0; i < 1; i++) {
-    byte *b = (byte *)lxHead;
-    UDP.beginPacket(bcastAddr, lxPort);
-    UDP.write(b, sizeof(lx_protocol_header_t));
-    UDP.endPacket();
+// 	for (int j = 0; j < 20; j++) {
 
-    delay(500);
-  }
+// 	int sizePacket = UDP.parsePacket();
+// 	if (sizePacket) {
+// 		UDP.read(packetBuffer, sizePacket);
+// 		byte target_addr[SIZE_OF_MAC];
+// 		memcpy(target_addr, packetBuffer + 8, SIZE_OF_MAC);
+// 		// Print MAC
+// 		for (int i = 0; i < sizeof(target_addr); i++) {
+// 		Serial.print(target_addr[i - 1], HEX);
+// 		Serial.print(" ");
+// 		}
+// 		Serial.println();
 
-  free(lxHead);
+// 		// Check if this device is new
+// 		uint8_t previouslyKnownDevice = 0;
+// 		for (int i = 0; i < LX_DEVICES; i++) {
+// 		if (!memcmp(lxDevices[i], target_addr, SIZE_OF_MAC)) {
+// 			Serial.println("Previously seen target");
+// 			previouslyKnownDevice = 1;
+// 			break;
+// 		}
+// 		}
+// 		// Store new devices
+// 		if (!previouslyKnownDevice) {
+// 		lxDevicesAddr[LX_DEVICES] = (uint32_t)UDP.remoteIP();
 
-  for (int j = 0; j < 20; j++) {
+// 		Serial.println(UDP.remoteIP());
+// 		memcpy(lxDevices[LX_DEVICES++], target_addr, SIZE_OF_MAC);
+// 		Serial.print("Storing device as LX_DEVICE ");
+// 		Serial.println(LX_DEVICES);
+// 		}
+// 	}
+// 	delay(20);
+// 	}
+// }
 
-    int sizePacket = UDP.parsePacket();
-    if (sizePacket) {
-      UDP.read(packetBuffer, sizePacket);
-      byte target_addr[SIZE_OF_MAC];
-      memcpy(target_addr, packetBuffer + 8, SIZE_OF_MAC);
-      // Print MAC
-      for (int i = 0; i < sizeof(target_addr); i++) {
-        Serial.print(target_addr[i - 1], HEX);
-        Serial.print(" ");
-      }
-      Serial.println();
+// void lxPower(uint16_t level) {
+//   for (int i = 0; i < LX_DEVICES; i++) {
+//     /* Set target_addr from know lxDevices */
+//     byte target_addr[8] = {0};
+//     memcpy(target_addr, lxDevices[i], SIZE_OF_MAC);
 
-      // Check if this device is new
-      uint8_t previouslyKnownDevice = 0;
-      for (int i = 0; i < LX_DEVICES; i++) {
-        if (!memcmp(lxDevices[i], target_addr, SIZE_OF_MAC)) {
-          Serial.println("Previously seen target");
-          previouslyKnownDevice = 1;
-          break;
-        }
-      }
-      // Store new devices
-      if (!previouslyKnownDevice) {
-        lxDevicesAddr[LX_DEVICES] = (uint32_t)UDP.remoteIP();
+//     /* Build payload */
+//     uint8_t type = 117;
+//     uint32_t duration = 500;
 
-        Serial.println(UDP.remoteIP());
-        memcpy(lxDevices[LX_DEVICES++], target_addr, SIZE_OF_MAC);
-        Serial.print("Storing device as LX_DEVICE ");
-        Serial.println(LX_DEVICES);
-      }
-    }
-    delay(20);
-  }
-}
+//     lx_protocol_header_t *lxHead;
+//     lxHead = (lx_protocol_header_t *)calloc(1, sizeof(lx_protocol_header_t));
+//     lxMakeFrame(lxHead, sizeof(lx_set_power_t), 0, target_addr, type);
 
-void lxPower(uint16_t level) {
-  for (int i = 0; i < LX_DEVICES; i++) {
-    /* Set target_addr from know lxDevices */
-    byte target_addr[8] = {0};
-    memcpy(target_addr, lxDevices[i], SIZE_OF_MAC);
+//     lx_set_power_t *lxSetPower;
+//     lxSetPower = (lx_set_power_t *)calloc(1, sizeof(lx_set_power_t));
+//     lxSetPower->duration = 500;
+//     lxSetPower->level = level;
 
-    /* Build payload */
-    uint8_t type = 117;
-    uint32_t duration = 500;
+//     UDP.beginPacket(lxDevicesAddr[i], lxPort);
+//     byte *b = (byte *)lxHead;
+//     UDP.write(b, sizeof(lx_protocol_header_t));
+//     b = (byte *)lxSetPower;
+//     UDP.write(b, sizeof(lx_set_power_t));
+//     UDP.endPacket();
 
-    lx_protocol_header_t *lxHead;
-    lxHead = (lx_protocol_header_t *)calloc(1, sizeof(lx_protocol_header_t));
-    lxMakeFrame(lxHead, sizeof(lx_set_power_t), 0, target_addr, type);
+//     free(lxSetPower);
+//     free(lxHead);
 
-    lx_set_power_t *lxSetPower;
-    lxSetPower = (lx_set_power_t *)calloc(1, sizeof(lx_set_power_t));
-    lxSetPower->duration = 500;
-    lxSetPower->level = level;
-
-    UDP.beginPacket(lxDevicesAddr[i], lxPort);
-    byte *b = (byte *)lxHead;
-    UDP.write(b, sizeof(lx_protocol_header_t));
-    b = (byte *)lxSetPower;
-    UDP.write(b, sizeof(lx_set_power_t));
-    UDP.endPacket();
-
-    free(lxSetPower);
-    free(lxHead);
-
-    Serial.println(lxDevicesAddr[i]);
-  }
-  Serial.println();
-}
+//     Serial.println(lxDevicesAddr[i]);
+//   }
+//   Serial.println();
+// }
 
 void isr_p2() {
-  buttonPushed = 1;
-  interuptPin = 2;
+	buttonPushed = 2;
 }
 
 void isr_p0() {
-  buttonPushed = 1;
-  interuptPin = 0;
+	buttonPushed = 0;
 }
 
-void isr_timeout() { buttonTimeout = 1; }
-
 void setup() {
+	Serial.begin(230400);
+	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
-  uint8_t zeros[SIZE_OF_MAC] = {0};
-  for (int i = 0; i < MAX_LX_DEVICES; i++) {
-    memcpy(lxDevices[i], zeros, SIZE_OF_MAC);
-  }
+	while (WiFi.status() != WL_CONNECTED) {
+	#if DEBUG
+	delay(500);
+	Serial.print(".");
+	#endif
+	}
 
-  Serial.begin(230400);
-  delay(10);
+	#if DEBUG
+	Serial.println();
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+	#endif
 
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
+	bcastAddr = WiFi.localIP();
+	bcastAddr[3] = 255;
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	UDP.begin(LIFX_UDP_PORT);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+	/* Setup Interupts */
+	pinMode(2, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(2), isr_p2, FALLING);
+	pinMode(0, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(0), isr_p0, FALLING);
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	Serial.println();
+}
 
-  bcastAddr = WiFi.localIP();
-  bcastAddr[3] = 255;
+lx_protocol_header_t discovery_header = {
+	sizeof(lx_protocol_header_t),
+	LIFX_HEADER_PROTOCOL,
+	LIFX_HEADER_ADDRESSABLE,
+	0, // only for discovery header
+	LIFX_HEADER_ORIGIN,
+	LIFX_HEADER_SOURCE,
+	{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+	{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+	LIFX_HEADER_RES_REQ,
+	LIFX_HEADER_ACK_REQ,
+	0,
+	LIFX_MSG_GET_SERVICE
+};
 
-  /* Setup Interupts */
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), isr_p2, FALLING);
-  pinMode(0, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(0), isr_p0, FALLING);
+lx_protocol_header_t read_header;
+
+static void printHeader(lx_protocol_header_t* head) {
+	Serial.printf("Size: %d\n", head->size);
+	Serial.printf("Protocol: %d\n", head->protocol);
+	Serial.printf("Addressable: %d\n", head->addressable);
+	Serial.printf("Tagged: %d\n", head->tagged);
+	Serial.printf("Origin: %d\n", head->origin);
+	Serial.printf("Source: %d\n", head->source);
+	Serial.printf("Target: ");
+	for (uint8_t i = 0; i < LIFX_HEADER_TARGET_LEN; ++i) {
+		Serial.printf("%02X ", head->target[i]);
+	}
+	Serial.println();
+	Serial.printf("Reserved: ");
+	for (uint8_t i = 0; i < LIFX_HEADER_RESERVED_LEN; ++i) {
+		Serial.printf("%02X ", head->reserved[i]);
+	}
+	Serial.println();
+	Serial.printf("Response Required: %d\n", head->res_required);
+	Serial.printf("Acknowledgement Required: %d\n", head->ack_required);
+	Serial.printf("Sequence: %d\n", head->sequence);
+	Serial.printf("Type: %d\n", head->type);
+	Serial.println();
 }
 
 void loop() {
-  interrupts();
+	interrupts();
+	while (buttonPushed==-1) {
+		delay(50); // Spinning causes WDT to reset
+	}
+	noInterrupts();
 
-  /* Button Watch*/
-  unsigned short buttonPushCount = 0;
+	Serial.print("Pushed: ");
+	Serial.print(buttonPushed, DEC);
+	Serial.println();
+	Serial.println();
 
-  if (buttonPushed) {
-    ticker.attach(0.4, isr_timeout);
-    while (buttonTimeout == 0) {
-      if (buttonPushed) {
-        noInterrupts();
-        buttonPushCount++;
-        buttonPushed = 0;
-        delay(120);
-        interrupts();
-      }
-      // Spinning here crashed the esp for some reason.
-      delay(20);
-    }
-    noInterrupts();
-    ticker.detach();
-    buttonTimeout = 0;
+	if (buttonPushed==0) {
+		Serial.println("SEND HEADER");
+		printHeader(&discovery_header);
 
-    /* Button watching finished, do something! */
-    char strInteruptPin[8];
-    sprintf(strInteruptPin, "Pin %d ", interuptPin);
-    Serial.print(strInteruptPin);
+		UDP.beginPacket(bcastAddr, LIFX_UDP_PORT);
+		char* discoveryHead_ptr = (char*) (&discovery_header);
+    	UDP.write(discoveryHead_ptr, sizeof(lx_protocol_header_t));
+    	UDP.endPacket();
 
-    if (buttonPushCount == 1 && !digitalRead(interuptPin)) {
-      Serial.println("LONG");
-      /* Pin 0, Long Push : Discovery */
-      if (interuptPin == 0) {
-        Serial.println("Begin LX Discovery");
-        lxDiscovery();
-        delay(1000);
-        lxDiscovery();
-        /* Release UDP resources */
-        UDP.stopAll();
-        Serial.println("Finished LX Discovery");
-      }
+		delay(500);
 
-    } else {
-      Serial.printf("sizeof(lx_protocol_header_t): %d\n", sizeof(lx_protocol_header_t));
-      Serial.println(buttonPushCount);
-      /* Pin 0, Multiple Clicks : Power Off */
-      if (interuptPin == 0 && buttonPushCount > 1) {
-        lxPower(0);
-      }
-      /* Pin 0, Single Click : Power On */
-      if (interuptPin == 0 && buttonPushCount == 1) {
-        lxPower(65534);
-      }
-    }
-  }
-  delay(50);
+		int packetSize = UDP.parsePacket();
+		char* readHead_ptr = (char*) (&read_header);
+		int readSize = (packetSize < sizeof(lx_protocol_header_t)) ? packetSize : sizeof(lx_protocol_header_t);
+		UDP.read(readHead_ptr, readSize);
+
+		Serial.println("RECEIVE HEADER");
+		printHeader(&read_header);
+	}
+	else if (buttonPushed==2) {
+		Serial.println("TWO!");
+	}
+
+	delay(500);
+	buttonPushed = -1;
 }
---
+++ main.cpp.ex
@@ -0,0 +1,307 @@
+// https://github.com/aijayadams/esp8266-lifx
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <WiFiUdp.h>
+
+// Header
+#define LIFX_HEADER_TARGET_LEN 8 
+// Header Constants
+#define LIFX_HEADER_PROTOCOL 1024 // Must be 1024
+#define LIFX_HEADER_ADDRESSABLE 1 // Indicates header includes target address, must be 1
+#define LIFX_HEADER_ORIGIN 0      // Must be 0 
+// Header Parameters
+#define LIFX_HEADER_SOURCE 3549
+#define LIFX_HEADER_RES_REQ 0 // Have light send back an acknowledgement. Not used in this code.
+#define LIFX_HEADER_ACK_REQ 0 // Request a response from the light. Not used in this code.
+
+const char *WIFI_SSID = "125 2.4GHz";
+const char *WIFI_PASSWORD = "fivetimesfive";
+
+const uint8_t MAX_LX_DEVICES = 16;
+uint8_t LX_DEVICES = 0;
+
+const uint8_t SIZE_OF_MAC = 6;
+
+byte lxDevices[MAX_LX_DEVICES][SIZE_OF_MAC];
+IPAddress lxDevicesAddr[MAX_LX_DEVICES];
+
+IPAddress bcastAddr(255, 255, 255, 255);
+int lxPort = 56700;
+
+WiFiUDP UDP;
+
+uint8_t buttonPushed = 0;
+uint8_t buttonTimeout = 0;
+uint8_t interuptPin = 0;
+
+#pragma pack(push, 1)
+typedef struct {
+  /* frame */
+  uint16_t size;            // Size of entire message in bytes including this field
+  uint16_t protocol : 12;   // Protocol number: must be 1024 (decimal)
+  uint8_t addressable : 1;  // Message includes a target address: must be one (1)
+  uint8_t tagged : 1;       // Determines usage of the Frame Address target field
+  uint8_t origin : 2;       // Message origin indicator: must be zero (0)
+  uint32_t source;          // Source identifier: unique value set by the client, used by responses
+  /* frame address */
+  uint8_t target[8];        // 6 byte device address (MAC address) or zero (0) means all devices
+  uint8_t reserved[6];      // Must all be zero (0)
+  uint8_t res_required : 1; // Response message required
+  uint8_t ack_required : 1; // Acknowledgement message required
+  uint8_t : 6;              // Reserved
+  uint8_t sequence;         // Wrap around message sequence number
+  /* protocol header */
+  uint64_t : 64;            // Reserved
+  uint16_t type;            // Message type determines the payload being used
+  uint16_t : 16;            // Reserved
+  /* variable length payload follows */
+} lx_protocol_header_t;
+#pragma pack(pop)
+
+// lx_protocol_header_t testHeader = {
+//   .size = 36,
+//   .protocol = 1024,
+//   .addressable = 1,
+//   .tagged = 
+// };
+
+#pragma pack(push, 1)
+typedef struct {
+  /* set power */
+  uint16_t level;
+  uint32_t duration;
+} lx_set_power_t;
+#pragma pack(pop)
+
+byte packetBuffer[128];
+Ticker ticker;
+
+// Payload bytes: length of the actual message
+// Tagged: should be 1 unless discovering LIFX devices
+// TargetMAC: MAC address array
+// MessageType: ID of the message included in this packet
+void lxMakeFrame(lx_protocol_header_t *lxHead, uint8_t payloadBytes,
+                 uint8_t tagged, uint8_t *targetMAC, uint16_t messageType,
+                 uint8_t sequenceNum=0) {
+  /* frame */
+  lxHead->size = sizeof(lx_protocol_header_t) + payloadBytes;
+  lxHead->protocol = LIFX_HEADER_PROTOCOL; // Must be 1024
+  lxHead->addressable = LIFX_HEADER_ADDRESSABLE; // Must be 1
+  lxHead->tagged = tagged;
+  lxHead->origin = LIFX_HEADER_ORIGIN; // Must be 0
+  lxHead->source = LIFX_HEADER_SOURCE; // Identifies the ESP8266, LIFX bulb sends responses to this number
+
+  /* frame address */
+  for (uint8_t i = 0; i < LIFX_HEADER_TARGET_LEN; i++) {
+    lxHead->target[i] = targetMAC[i];
+  }
+  lxHead->res_required = LIFX_HEADER_RES_REQ;
+  lxHead->ack_required = LIFX_HEADER_ACK_REQ;
+  lxHead->sequence = sequenceNum;
+
+  /* protocol header */
+  lxHead->type = messageType;
+}
+
+void lxDiscovery() {
+  /* Build lxDiscovery payload */
+  byte target_addr[8] = {0};
+  lx_protocol_header_t *lxHead;
+  lxHead = (lx_protocol_header_t *)calloc(1, sizeof(lx_protocol_header_t));
+  lxMakeFrame(lxHead, 0, 1, target_addr, 2);
+
+  /* Start listening for responses */
+  UDP.begin(4097);
+  delay(500);
+  /* Send a couple of discovery packets out to the network*/
+  for (int i = 0; i < 1; i++) {
+    byte *b = (byte *)lxHead;
+    UDP.beginPacket(bcastAddr, lxPort);
+    UDP.write(b, sizeof(lx_protocol_header_t));
+    UDP.endPacket();
+
+    delay(500);
+  }
+
+  free(lxHead);
+
+  for (int j = 0; j < 20; j++) {
+
+    int sizePacket = UDP.parsePacket();
+    if (sizePacket) {
+      UDP.read(packetBuffer, sizePacket);
+      byte target_addr[SIZE_OF_MAC];
+      memcpy(target_addr, packetBuffer + 8, SIZE_OF_MAC);
+      // Print MAC
+      for (int i = 0; i < sizeof(target_addr); i++) {
+        Serial.print(target_addr[i - 1], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+
+      // Check if this device is new
+      uint8_t previouslyKnownDevice = 0;
+      for (int i = 0; i < LX_DEVICES; i++) {
+        if (!memcmp(lxDevices[i], target_addr, SIZE_OF_MAC)) {
+          Serial.println("Previously seen target");
+          previouslyKnownDevice = 1;
+          break;
+        }
+      }
+      // Store new devices
+      if (!previouslyKnownDevice) {
+        lxDevicesAddr[LX_DEVICES] = (uint32_t)UDP.remoteIP();
+
+        Serial.println(UDP.remoteIP());
+        memcpy(lxDevices[LX_DEVICES++], target_addr, SIZE_OF_MAC);
+        Serial.print("Storing device as LX_DEVICE ");
+        Serial.println(LX_DEVICES);
+      }
+    }
+    delay(20);
+  }
+}
+
+void lxPower(uint16_t level) {
+  for (int i = 0; i < LX_DEVICES; i++) {
+    /* Set target_addr from know lxDevices */
+    byte target_addr[8] = {0};
+    memcpy(target_addr, lxDevices[i], SIZE_OF_MAC);
+
+    /* Build payload */
+    uint8_t type = 117;
+    uint32_t duration = 500;
+
+    lx_protocol_header_t *lxHead;
+    lxHead = (lx_protocol_header_t *)calloc(1, sizeof(lx_protocol_header_t));
+    lxMakeFrame(lxHead, sizeof(lx_set_power_t), 0, target_addr, type);
+
+    lx_set_power_t *lxSetPower;
+    lxSetPower = (lx_set_power_t *)calloc(1, sizeof(lx_set_power_t));
+    lxSetPower->duration = 500;
+    lxSetPower->level = level;
+
+    UDP.beginPacket(lxDevicesAddr[i], lxPort);
+    byte *b = (byte *)lxHead;
+    UDP.write(b, sizeof(lx_protocol_header_t));
+    b = (byte *)lxSetPower;
+    UDP.write(b, sizeof(lx_set_power_t));
+    UDP.endPacket();
+
+    free(lxSetPower);
+    free(lxHead);
+
+    Serial.println(lxDevicesAddr[i]);
+  }
+  Serial.println();
+}
+
+void isr_p2() {
+  buttonPushed = 1;
+  interuptPin = 2;
+}
+
+void isr_p0() {
+  buttonPushed = 1;
+  interuptPin = 0;
+}
+
+void isr_timeout() { buttonTimeout = 1; }
+
+void setup() {
+
+  uint8_t zeros[SIZE_OF_MAC] = {0};
+  for (int i = 0; i < MAX_LX_DEVICES; i++) {
+    memcpy(lxDevices[i], zeros, SIZE_OF_MAC);
+  }
+
+  Serial.begin(230400);
+  delay(10);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  bcastAddr = WiFi.localIP();
+  bcastAddr[3] = 255;
+
+  /* Setup Interupts */
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), isr_p2, FALLING);
+  pinMode(0, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(0), isr_p0, FALLING);
+}
+
+void loop() {
+  interrupts();
+
+  /* Button Watch*/
+  unsigned short buttonPushCount = 0;
+
+  if (buttonPushed) {
+    ticker.attach(0.4, isr_timeout);
+    while (buttonTimeout == 0) {
+      if (buttonPushed) {
+        noInterrupts();
+        buttonPushCount++;
+        buttonPushed = 0;
+        delay(120);
+        interrupts();
+      }
+      // Spinning here crashed the esp for some reason.
+      delay(20);
+    }
+    noInterrupts();
+    ticker.detach();
+    buttonTimeout = 0;
+
+    /* Button watching finished, do something! */
+    char strInteruptPin[8];
+    sprintf(strInteruptPin, "Pin %d ", interuptPin);
+    Serial.print(strInteruptPin);
+
+    if (buttonPushCount == 1 && !digitalRead(interuptPin)) {
+      Serial.println("LONG");
+      /* Pin 0, Long Push : Discovery */
+      if (interuptPin == 0) {
+        Serial.println("Begin LX Discovery");
+        lxDiscovery();
+        delay(1000);
+        lxDiscovery();
+        /* Release UDP resources */
+        UDP.stopAll();
+        Serial.println("Finished LX Discovery");
+      }
+
+    } else {
+      Serial.printf("sizeof(lx_protocol_header_t): %d\n", sizeof(lx_protocol_header_t));
+      Serial.println(buttonPushCount);
+      /* Pin 0, Multiple Clicks : Power Off */
+      if (interuptPin == 0 && buttonPushCount > 1) {
+        lxPower(0);
+      }
+      /* Pin 0, Single Click : Power On */
+      if (interuptPin == 0 && buttonPushCount == 1) {
+        lxPower(65534);
+      }
+    }
+  }
+  delay(50);
+}
